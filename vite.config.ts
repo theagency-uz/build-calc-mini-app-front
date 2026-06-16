153c8849c6bc6881ed9e786a79509ad8be8211b6
DmitriiFRS <dmitriifrs@gmail.com>
2026-06-16 13:29:46 +0500

    added docs

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,16 @@
-import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
-import tailwindcss from '@tailwindcss/vite'
-import { fileURLToPath } from 'node:url'
+import { defineConfig } from "vite";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import babel from "@rolldown/plugin-babel";
+import tailwindcss from "@tailwindcss/vite";
+import { fileURLToPath } from "node:url";
 
 // https://vite.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
-  plugins: [
-    tailwindcss(),
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
-})
+	base: "/build-calc-mini-app-front/",
+	resolve: {
+		alias: {
+			"@": fileURLToPath(new URL("./src", import.meta.url)),
+		},
+	},
+	plugins: [tailwindcss(), react(), babel({ presets: [reactCompilerPreset()] })],
+});
